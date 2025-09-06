2b261cc988c6fd6e6aa915e3d68375c32fa59a22
Daigo Tokunaga <tokunaga.c.daigo@example.com>
2025-09-06 21:35:49 +0900

    fix: assert_valid_keys に対応

--- lib/ridgepole-ext-tidb.rb
+++ lib/ridgepole-ext-tidb.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 require_relative 'ridgepole/ext/tidb'
+
+# Ridgepoleが使用される前に拡張を適用
 Ridgepole::Ext::Tidb.setup!
+
+# ActiveRecordアダプタが利用可能になったときに自動拡張を実行
+if defined?(ActiveRecord::Base)
+  Ridgepole::Ext::Tidb.extend_activerecord_adapters
+else
+  # ActiveRecordが後でロードされる場合に備えてフックを設定
+  ActiveSupport.on_load(:active_record) do
+    Ridgepole::Ext::Tidb.extend_activerecord_adapters
+  end
+end
